daa811f3a7cacc7a92d9af35f4106db2d704e2fa
ONeil <evgenprogrammer@gmail.com>
2025-08-23 15:43:12 +0300

    add graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	_ "github.com/EvgenyiK/subscription-service/cmd/docs"
 	"github.com/EvgenyiK/subscription-service/internal/handlers"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/EvgenyiK/subscription-service/internal/config"
 	"github.com/EvgenyiK/subscription-service/internal/repository"
@@ -37,9 +42,35 @@ func main() {
 
 	router := server.NewRouter(h)
 
-	log.Printf("Server starting on port %s...", cfg.ServerPort)
-	err = http.ListenAndServe(":"+cfg.ServerPort, router)
-	if err != nil {
-		log.Fatal(err)
+	serverAddr := ":" + cfg.ServerPort
+	srv := &http.Server{
+		Addr:    serverAddr,
+		Handler: router,
 	}
+
+	go func() {
+		log.Printf("Server starting on port %s...", cfg.ServerPort)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not listen on %s: %v", serverAddr, err)
+		}
+	}()
+
+	// Создаем канал для ловли системных сигналов
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	// Ожидаем сигнала
+	sig := <-sigs
+	log.Printf("Получен сигнал %s. Начинаем graceful shutdown...", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("Ошибка при graceful shutdown: %v", err)
+	} else {
+		log.Println("Сервер успешно остановлен")
+	}
+
+	log.Println("Выход из программы")
 }
